0e99ddc4a1e92883068bd77c817dab9aa8a30f1f
cakia <cakia99999@gmail.com>
2022-05-08 15:36:21 +0700

    Update mysql createPool

--- server/db.ts
+++ server/db.ts
@@ -4,17 +4,11 @@ import mysql from "mysql";
 const dbConfig = require("./db.config");
 
 // Create a connection to the database
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB,
-});
-
-// open the MySQL connection
-connection.connect((error) => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
 });
 
 export const dbConn = connection;
