023f3b4e6a9a534b5c9cdd5c75633b6f145000d9
cakia <cakia99999@gmail.com>
2022-06-11 15:02:32 +0700

    mexclite scraper

---
+++ scraper/sql/mexclite.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `mexclite` (
+  `token` varchar(45) NOT NULL,
+  `tokenName` varchar(45) DEFAULT NULL,
+  `leverageLimit` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
