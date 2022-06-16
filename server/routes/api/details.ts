64b5ec91468ae48ebf180aca6b5505ecf1906d45
cakia <cakia99999@gmail.com>
2022-06-16 11:23:58 +0700

    Fix details bug

--- server/routes/api/details.ts
+++ server/routes/api/details.ts
@@ -43,108 +43,119 @@ export default async (req: Request, res: Response): Promise<Response> => {
         token.network === "BSC" &&
         (token.tpxneedupdate === 1 || token.tpxneedupdate === null)
       ) {
-        const price = await Moralis.Web3API.token.getTokenPrice({
-          chain: "bsc",
-          address: token.address,
-        });
-
-        tokenBase = {
-          ...tokenBase,
-          price: price.usdPrice,
-        };
-
-        const insertHistoryQuery = `insert ignore into ${db}.tokenhistory values (${dbConn.escape(
-          token.address
-        )}, 'BSC', NOW(), ${price.usdPrice});`;
-
-        // console.log(insertHistoryQuery);
-        await new Promise((res, rej) => {
-          dbConn.query(insertHistoryQuery, function (err, result) {
-            if (err) return rej(err);
-            return res(result);
-          });
-        });
-
-        const insertQuery = `insert into ${db}.tokenprice values (${dbConn.escape(
-          token.address
-        )}, 'BSC', ${
-          price.usdPrice
-        }, null, NOW(), null) on DUPLICATE KEY UPDATE price = ${
-          price.usdPrice
-        }, updatedAt = NOW();`;
-
-        // console.log(insertQuery);
-        await new Promise((res, rej) => {
-          dbConn.query(insertQuery, function (err, result) {
-            if (err) return rej(err);
-            return res(result);
-          });
-        });
-
-        const dateNow = token.tpxnow;
-        const openpriceNeedUpdate = (() => {
-          if (token.tpxopenpricedate === null) return true;
-
-          const openpriceStr = token.tpxopenpricedate.split(" ")[0];
-          if (openpriceStr === dateNow) return false;
-
-          return true;
-        })();
-
-        if (openpriceNeedUpdate) {
-          const block = await Moralis.Web3API.native.getDateToBlock({
-            chain: "bsc",
-            date: dateNow,
-          });
-          // console.log("block", block);
-          const todayPrice = await Moralis.Web3API.token.getTokenPrice({
+        try {
+          const price = await Moralis.Web3API.token.getTokenPrice({
             chain: "bsc",
             address: token.address,
-            to_block: block.block,
           });
-          // console.log("todayPrice", todayPrice);
 
-          if (todayPrice.usdPrice) {
+          tokenBase = {
+            ...tokenBase,
+            price: price.usdPrice,
+          };
+
+          const insertHistoryQuery = `insert ignore into ${db}.tokenhistory values (${dbConn.escape(
+            token.address
+          )}, 'BSC', NOW(), ${price.usdPrice});`;
+
+          // console.log(insertHistoryQuery);
+          await new Promise((res, rej) => {
+            dbConn.query(insertHistoryQuery, function (err, result) {
+              if (err) return rej(err);
+              return res(result);
+            });
+          });
+
+          const insertQuery = `insert into ${db}.tokenprice values (${dbConn.escape(
+            token.address
+          )}, 'BSC', ${
+            price.usdPrice
+          }, null, NOW(), null) on DUPLICATE KEY UPDATE price = ${
+            price.usdPrice
+          }, updatedAt = NOW();`;
+
+          // console.log(insertQuery);
+          await new Promise((res, rej) => {
+            dbConn.query(insertQuery, function (err, result) {
+              if (err) return rej(err);
+              return res(result);
+            });
+          });
+
+          const dateNow = token.tpxnow;
+          const openpriceNeedUpdate = (() => {
+            if (token.tpxopenpricedate === null) return true;
+
+            const openpriceStr = token.tpxopenpricedate.split(" ")[0];
+            if (openpriceStr === dateNow) return false;
+
+            return true;
+          })();
+
+          if (openpriceNeedUpdate) {
+            const block = await Moralis.Web3API.native.getDateToBlock({
+              chain: "bsc",
+              date: dateNow,
+            });
+            // console.log("block", block);
+            const todayPrice = await Moralis.Web3API.token.getTokenPrice({
+              chain: "bsc",
+              address: token.address,
+              to_block: block.block,
+            });
+            // console.log("todayPrice", todayPrice);
+
+            if (todayPrice.usdPrice) {
+              const priceChangePct =
+                ((price.usdPrice - todayPrice.usdPrice) / todayPrice.usdPrice) *
+                100;
+
+              tokenBase = {
+                ...tokenBase,
+                pricechange: priceChangePct,
+              };
+
+              const insertOpenHistoryQuery = `insert ignore into ${db}.tokenhistory values (${dbConn.escape(
+                token.address
+              )}, 'BSC', '${dateNow}', ${todayPrice.usdPrice});`;
+
+              // console.log(insertOpenHistoryQuery);
+              await new Promise((res, rej) => {
+                dbConn.query(insertOpenHistoryQuery, function (err, result) {
+                  if (err) return rej(err);
+                  return res(result);
+                });
+              });
+
+              const updateQuery = `update ${db}.tokenprice
+              set openprice = ${todayPrice.usdPrice}, openpricedate = '${dateNow}'
+              where address = '${token.address}' and network = 'BSC';`;
+
+              // console.log(updateQuery);
+              await new Promise((res, rej) => {
+                dbConn.query(updateQuery, function (err, result) {
+                  if (err) return rej(err);
+                  return res(result);
+                });
+              });
+            }
+          } else {
             const priceChangePct =
-              ((price.usdPrice - todayPrice.usdPrice) / todayPrice.usdPrice) *
+              ((price.usdPrice - token.tpxopenprice) / token.tpxopenprice) *
               100;
 
             tokenBase = {
               ...tokenBase,
               pricechange: priceChangePct,
             };
-
-            const insertOpenHistoryQuery = `insert ignore into ${db}.tokenhistory values (${dbConn.escape(
-              token.address
-            )}, 'BSC', '${dateNow}', ${todayPrice.usdPrice});`;
-
-            // console.log(insertOpenHistoryQuery);
-            await new Promise((res, rej) => {
-              dbConn.query(insertOpenHistoryQuery, function (err, result) {
-                if (err) return rej(err);
-                return res(result);
-              });
-            });
-
-            const updateQuery = `update ${db}.tokenprice
-              set openprice = ${todayPrice.usdPrice}, openpricedate = '${dateNow}'
-              where address = '${token.address}' and network = 'BSC';`;
-
-            // console.log(updateQuery);
-            await new Promise((res, rej) => {
-              dbConn.query(updateQuery, function (err, result) {
-                if (err) return rej(err);
-                return res(result);
-              });
-            });
           }
-        } else {
-          const priceChangePct =
-            ((price.usdPrice - token.tpxopenprice) / token.tpxopenprice) * 100;
-
+        } catch {
           tokenBase = {
             ...tokenBase,
-            pricechange: priceChangePct,
+            price: token.tpxprice,
+            pricechange:
+              ((token.tpxprice - token.tpxopenprice) / token.tpxopenprice) *
+              100,
           };
         }
       } else if (token.network === "BSC") {
